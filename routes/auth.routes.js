2a0ac0aa9f80c103e613b97788dde638e7d65708
joeCodes05 <nathancodes05@gmail.com>
2024-01-27 19:29:31 +0100

    auth routes for user CRUD functionality

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,19 @@
 const express = require("express");
-const { createUser, loginUser } = require("../controllers/auth.controller");
+const {
+  createUser,
+  loginUser,
+  updateUserDetails,
+  deleteUsers,
+  getAllUsers,
+} = require("../controllers/auth.controller");
 
 const router = express.Router();
 
 // auth api routes
+router.get("/", getAllUsers);
 router.post("/create-user", createUser);
 router.post("/login-user", loginUser);
+router.put("/:id", updateUserDetails);
+router.delete("/:id", deleteUsers);
 
 module.exports = router;
