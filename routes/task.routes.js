5122922b93d59eb55b1cc60067b402f5a24d023a
joeCodes05 <nathancodes05@gmail.com>
2024-01-28 09:55:14 +0100

    worked on update task route.

--- routes/task.routes.js
+++ routes/task.routes.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { getAllTasks, createTasks } = require("../controllers/tasks.controller");
+const {
+  getAllTasks,
+  createTasks,
+  updateTask,
+} = require("../controllers/tasks.controller");
 
 const router = express.Router();
 
 router.get("/", getAllTasks);
 router.post("/create-task", createTasks);
+router.put("/:id", updateTask);
 
 module.exports = router;
