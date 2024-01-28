8fc2fa4304820ac6db0599cbfd6dba50f2115328
joeCodes05 <nathancodes05@gmail.com>
2024-01-28 06:41:44 +0100

    minor changes to code that creates tasks table

--- config/database.js
+++ config/database.js
@@ -13,14 +13,28 @@ db.connect((err) => {
 
   console.log("Database connection successfull");
 
-  //   create tables
+  //   creating users tables if it does not exists
   db.query(`CREATE TABLE IF NOT EXISTS users (
-        user_id INT PRIMARY KEY AUTO_INCREMENT,
+        user_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
         full_name VARCHAR(255) NOT NULL,
         email VARCHAR(255) NOT NULL,
         password VARCHAR(255) NOT NULL,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    )CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci, ENGINE=InnoDB`);
+
+  // creating tasks table if it does not exists
+  db.query(`CREATE TABLE IF NOT EXISTS tasks (
+      task_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+      user_id INT UNSIGNED NOT NULL,
+      title VARCHAR(255) NOT NULL,
+      content TEXT NOT NULL,
+      priority VARCHAR(255) NOT NULL,
+      category VARCHAR(255) NOT NULL,
+      completed TINYINT(0) NOT NULL,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+      FOREIGN KEY (user_id) REFERENCES users(user_id)
     )CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci, ENGINE=InnoDB`);
 });
 
