d1ee7261bcca312f9c32956662986b2b48e95286
joeCodes05 <nathancodes05@gmail.com>
2024-01-28 06:43:02 +0100

    creating task endpoint

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const authRoutes = require("./routes/auth.routes");
+const taskRoutes = require("./routes/task.routes");
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(express.urlencoded({ extended: false }));
 
 // routes
 app.use("/api/auth/", authRoutes);
+app.use("/api/tasks", taskRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port:${PORT}`));
